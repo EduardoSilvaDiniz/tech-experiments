2bfa7904beaaf462d01e9c81ed8c5a14792974a4
Eduardo Silva <eduardofox989@protonmail.com>
2024-06-08 13:06:34 -0300

    finished project

--- src/Fisico/Funcionario.java
+++ src/Fisico/Funcionario.java
@@ -1,18 +1,40 @@
 package Fisico;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Funcionario extends Pessoa{
   private String cargo, setor, tarefaExecutar;
 
-  public Funcionario() {}
+  public Funcionario() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Digite o nome do funcionario: ");
+    String nome = br.readLine();
+
+    System.out.println("digite cpf do funcionario: ");
+    String cpf = br.readLine();
+
+    System.out.println("digite a idade do funcionario: ");
+    int idade = Integer.parseInt(br.readLine());
+
+    super(nome, cpf, idade);
+
+    System.out.println("digite o cargo do funcionario: ");
+    this.cargo = br.readLine();
+
+    System.out.println("digite o setor do funcionario: ");
+    this.setor = br.readLine();
+
+    System.out.println("digite a tarefa a executar do funcionario: ");
+    this.tarefaExecutar = br.readLine();
+  }
 
   public Funcionario(String nome, String cpf, int idade, String cargo, String setor, String tarefaExecutar) {
     super(nome, cpf, idade);
     this.cargo = cargo;
     this.setor = setor;
-    this.tarefaExecutar = tarefaExecutar;
-  }
-
-  public void executarTarefa(String tarefaExecutar){
     this.tarefaExecutar = tarefaExecutar;
   }
 
@@ -47,8 +69,7 @@ public class Funcionario extends Pessoa{
   public String getTarefaExecutar() {
     return tarefaExecutar;
   }
-
-  public void setTarefaExecutar(String tarefaExecutar) {
+  public void executarTarefa(String tarefaExecutar){
     this.tarefaExecutar = tarefaExecutar;
   }
 }
--- src/Fisico/Professor.java
+++ src/Fisico/Professor.java
@@ -1,11 +1,13 @@
 package Fisico;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Professor extends Pessoa{
   private int quantidadeDeAulaDada;
   private double salario;
   private String disciplina;
-
-  public Professor() {}
 
   public Professor(String nome, String cpf, int idade, int salario, String disciplina) {
     super(nome, cpf, idade);
@@ -13,8 +15,25 @@ public class Professor extends Pessoa{
     this.disciplina = disciplina;
   }
 
-  public void darAula(int quantidadeDeAulaDada){
-    this.quantidadeDeAulaDada = quantidadeDeAulaDada;
+  public Professor() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Digite o nome do professor: ");
+    String nome = br.readLine();
+
+    System.out.println("digite cpf do professor: ");
+    String cpf = br.readLine();
+
+    System.out.println("digite a idade do professor: ");
+    int idade = Integer.parseInt(br.readLine());
+
+    super(nome, cpf, idade);
+
+    System.out.println("digite salario do professor: ");
+    this.salario = Integer.parseInt(br.readLine());
+
+    System.out.println("digite disciplina do professor: ");
+    this.disciplina = br.readLine();
   }
 
   @Override
@@ -41,8 +60,8 @@ public class Professor extends Pessoa{
     return quantidadeDeAulaDada;
   }
 
-  public void setQuantidadeDeAulaDada(int quantidadeDeAulaDada) {
-    this.quantidadeDeAulaDada = quantidadeDeAulaDada;
+  public void darAula(int quantidadeDeAulaDada){
+    this.quantidadeDeAulaDada = quantidadeDeAulaDada+1;
   }
 
   public String getDisciplina() {
