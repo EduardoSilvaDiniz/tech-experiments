ccd931f1fd2e5eb5ada8c92831fa92e5bef2221d
Eduardo Silva <eduardofox989@protonmail.com>
2024-08-27 12:02:38 -0300

    add pseudo OOP

--- binary-tree/binary-tree.h
+++ binary-tree/binary-tree.h
@@ -1,13 +1,21 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
-typedef struct node Node;
-Node *createNode(int value);
-void addNodeToTree(Node *node, Node *head);
-void searchFreeSpaceAndAddNode(Node *node, Node *head);
-void showAllTrees(Node *head);
-Node *searchNodeOnTree(Node *tree, int value);
-void removeNodeOnTree(Node *tree, int value);
+typedef struct binarytree {
+  struct node *base;
+  void (*add)(int data, struct node *curr);
+  void (*get)(int value, struct node *curr);
+  void (*removeData)(int value, struct node *curr);
+  void (*forEach)(struct node *curr);
+} BinaryTree;
+
+typedef struct node {
+  int data;
+  struct node *right;
+  struct node *left;
+} Node;
+
+BinaryTree *constructor(int value);
 Node *lastNodeExtremeRight(Node *node);
 Node *lastNodeExtremeLeft(Node *node);
 #endif // DEBUG
