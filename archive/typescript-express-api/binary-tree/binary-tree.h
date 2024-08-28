87b5a48b9d08ab856aa0e740e4ac7f175b2e1c41
Eduardo Silva <eduardofox989@protonmail.com>
2024-08-28 14:39:30 -0300

    change names and add new functions

--- archive/typescript-express-api/binary-tree/binary-tree.h
+++ archive/typescript-express-api/binary-tree/binary-tree.h
@@ -1,21 +1,24 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+typedef struct node Node;
 typedef struct binarytree {
-  struct node *base;
-  void (*add)(int data, struct node *curr);
-  struct node *(*get)(int value, struct node *curr);
-  void (*removeData)(int value, struct node *curr);
-  void (*forEach)(struct node *curr);
+  Node *root;
+  void (*insert)(int data, struct node *curr);
+  Node *(*search)(int value, struct node *curr);
+  Node *(*delete)(int value, struct node *curr);
+  void (*inorderTreeWalk)(struct node *curr);
 } BinaryTree;
 
 typedef struct node {
   int data;
-  struct node *right;
-  struct node *left;
+  Node *right;
+  Node *left;
+  Node *parent;
 } Node;
 
-BinaryTree *constructor(int value);
-Node *lastNodeExtremeRight(Node *node);
-Node *lastNodeExtremeLeft(Node *node);
+Node *minimum(Node *curr);
+Node *maximum(Node *curr);
+Node *sucessor(Node *curr);
+BinaryTree *constructor(int data);
 #endif // DEBUG
