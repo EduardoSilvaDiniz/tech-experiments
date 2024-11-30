b373fc637378610fbf600e4f1bd8087636f97bc2
Eduardo Silva <eduardofox989@protonmail.com>
2024-11-30 09:47:23 -0300

    result object is working, only implemented in insertionsort

--- archive/typescript-express-api/algorithms-for-ordering-and-searching/main/java/algorithms/base/Result.java
+++ archive/typescript-express-api/algorithms-for-ordering-and-searching/main/java/algorithms/base/Result.java
@@ -3,13 +3,19 @@ package main.java.algorithms.base;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Result<T> {
-  public List<T> list;
+public class Result {
+  public List<Integer> list;
   public int comparisons;
 
-  public Result(T item, int comparisons) {
+  public Result(Integer item, int comparisons) {
     list = new ArrayList<>();
     list.add(item);
+    this.comparisons = comparisons;
+  }
+
+  public Result(List<Integer> item, int comparisons) {
+    list = new ArrayList<>();
+    list.addAll(item);
     this.comparisons = comparisons;
   }
 }
--- archive/typescript-express-api/algorithms-for-ordering-and-searching/test/java/sort/InsertionSortUnitTest.java
+++ archive/typescript-express-api/algorithms-for-ordering-and-searching/test/java/sort/InsertionSortUnitTest.java
@@ -1,18 +1,36 @@
 package test.java.sort;
 
+import main.java.algorithms.base.Result;
+import main.java.algorithms.sort.BucketSort;
 import main.java.algorithms.sort.InsertionSort;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InsertionSortUnitTest {
+  private InsertionSort sorter;
+
+  @BeforeEach
+  public void setUp() {
+    sorter = new InsertionSort();
+  }
+
   @Test
   public void insertionSortTest() {
-    int[] list = {80, 50, 60, 30, 20, 10, 70, 0, 40, 500, 600, 602, 200, 15};
-    int[] listOrder = {0, 10, 15, 20, 30, 40, 50, 60, 70, 80, 200, 500, 600, 602};
-    int[] listSort = InsertionSort.sort(list);
 
-    Assertions.assertEquals(listOrder[0], listSort[0]);
-    Assertions.assertEquals(listOrder[listOrder.length / 2], listSort[listSort.length / 2]);
-    Assertions.assertEquals(listOrder[listOrder.length - 1], listSort[listSort.length - 1]);
+    List<Integer> unsorted = Arrays.asList(80,50,60,30,20,10,70,0,40,500,600,602,200,15);
+    List<Integer> expected = Arrays.asList(0,10,15,20,30,40,50,60,70,80,200,500,600,602);
+
+    Result actual = sorter.sort(unsorted);
+    int sizeMid = expected.size() / 2;
+
+    Assertions.assertEquals(expected.getFirst(), actual.list.getFirst());
+    Assertions.assertEquals(expected.get(sizeMid), actual.list.get(sizeMid));
+    Assertions.assertEquals(expected.getLast(), actual.list.getLast());
+    System.out.println(actual.comparisons + " Vezes que o algoritmo fez comparações");
   }
 }
