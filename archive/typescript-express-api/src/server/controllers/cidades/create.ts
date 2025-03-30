67d56939d637661de27b6b89f13b41b53448d8e6
Eduardo Silva <eduardofox989@protonmail.com>
2025-03-30 12:24:53 -0300

    feat: validation now accepts generic schema

--- archive/typescript-express-api/src/server/controllers/cidades/create.ts
+++ archive/typescript-express-api/src/server/controllers/cidades/create.ts
@@ -1,16 +1,20 @@
 import { Response, Request, RequestHandler } from "express";
-import { object, InferType, string, ObjectShape, Schema } from "yup";
+import { object, InferType, string, ObjectShape, Schema, number } from "yup";
 import { validation } from "../../shared/middleware";
 
-export const createValidation = validation({
-  body: object({
-    nome: string().required().min(3),
-    estado: string().required().min(2),
-  }),
-  query: object({
-    filter: string().required().min(3),
-  }),
-});
+export const createValidation = validation((getSchema) => ({
+  body: getSchema(
+    object({
+      nome: string().required().min(3),
+      estado: string().required().min(2),
+    }),
+  ),
+  query: getSchema(
+    object({
+      filter: string().required().min(3),
+    }),
+  ),
+}));
 
 export const create = (req: Request, res: Response) => {
   res.send("create");
